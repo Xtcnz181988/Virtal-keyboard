05569c1a32354dbc70aa84c8a51c7586599df18e
Xtcnz181988 <koblenev88@gmail.com>
2022-05-08 11:01:52 +0300

    feat: add webpack

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import 'normalize.css';
+import './style.css';
+import './assets/styles/main.css';
+import './assets/styles/fonts.css';
+
+import Control from './assets/Components/control';
+import TextArea from './assets/Components/textarea';
+import KeyBoard from './assets/Components/keyboard';
+import BUTTONS from './assets/Data/buttons';
+
+const textArea = new TextArea('textarea', 'textarea', 7, 100);
+textArea.createElement().render(document.body);
+textArea.printText('Hello'); // REMOVE AFTER
+
+const keyBoard = new KeyBoard(BUTTONS);
+keyBoard.createKeyBoard().createRowsKeys();
+console.log(keyBoard); // REMOVE AFTER
+
+const control = new Control(textArea, keyBoard);
+control.createListener();
+console.log(control);
+
+document.addEventListener('keydown', (e) => {
+    console.log(e)
+})
+
+// const textArea = new Component('textarea', 'textarea');
+// textArea.renderComponent(document.body);
+
+// import TextArea from './assets/Components/textarea';
+// import KeyBoard from './assets/Components/keyboard';
+
+// new TextArea('textarea', 'textarea').render(document.body);
+
+// const keyBoard = new KeyBoard();
+// console.log(keyBoard)
+// keyBoard.createKeyBoard()
+// console.log(textArea)
+// const keyBo = document.querySelector('.keyboard');
+
+// const row = new Component('div', 'row');
+// row.render(keyBo);
+
+// console.log(keyboard);
+
+// const wrapper = new Component ('div', 'wrapper');
+// wrapper.render(document.body);
+
